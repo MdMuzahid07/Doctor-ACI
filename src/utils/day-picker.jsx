5fbfefdcfccf9e0700acd97649a578a23a86a23a
mdmuzahid07 <mdmuzahid7396@gmail.com>
2023-08-07 15:50:30 +0600

    day picker function maked, and time picker component maked

---
+++ src/utils/day-picker.jsx
@@ -0,0 +1,26 @@
+import { format } from 'date-fns';
+import { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+const dayPicker = () => {
+    const [selected, setSelected] = useState(null);
+
+    let footer = <p>Please pick a day.</p>;
+    if (selected) {
+        footer = <p>You picked {format(selected, 'PP')}.</p>;
+    }
+
+    return (
+        <section className="p-5 bg-white rounded-lg">
+            <DayPicker
+                mode="single"
+                selected={selected}
+                onSelect={setSelected}
+                footer={footer}
+            />
+        </section>
+    );
+}
+
+export default dayPicker
